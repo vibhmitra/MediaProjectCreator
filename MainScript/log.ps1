b745cadc472152da982602b106b0f8b0f7acb329
vibhmitra <66880260+vibhmitra@users.noreply.github.com>
2025-05-06 18:02:09 +0000

    📈 Update init

---
+++ MainScript/log.ps1
@@ -0,0 +1,12 @@
+# TBD
+
+# This will be use to log the git-info.md file 
+
+# Maybe find the git-info.md file in the current directory
+
+# Example Prompt:
+# Ask Project  folder:  20250407-CoolCar-WIP  
+# Ask Revision  date  [default:  current  date]:  20250407
+# Ask Status  (WIP/Beta/C/R):  Beta  
+# Ask List  changes  (comma-separated):  Improved  animation  timing, added  engine  SF 
+# Notes: 
